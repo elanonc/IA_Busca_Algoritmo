489fa2afca3e8493eb00035293292172abfa2eae
Elano Nunes <53663166+elanonc@users.noreply.github.com>
2021-11-23 20:07:16 -0300

    At10-P.2

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@
 #define Urziceni 14
 #define Vaslui 15
 #define Mirsove 16
-#define lasi 17
+#define Lasi 17
 #define Neamt 18
 #define Eforie 19
 
@@ -30,29 +30,29 @@ int main()
 {
     int nVertice = 20; // numero de vertices
     Grafo g(nVertice);
-    g.adicionar(" ", " ", Arad, Zerind, 75);
-    g.adicionar(" ", " ", Arad, Sibiu, 140);
-    g.adicionar(" ", " ", Arad, Timisoare, 118);
-    g.adicionar(" ", " ", Zerind, Oradea, 71);
-    g.adicionar(" ", " ", Sibiu, Fagaras, 90);
-    g.adicionar(" ", " ", Sibiu, RimnicuVilcea, 80);
-    g.adicionar(" ", " ", Timisoare, Lugej, 111);
-    g.adicionar(" ", " ", Oradea, Sibiu, 151);
-    g.adicionar(" ", " ", Fagaras, Bucharest, 211);
-    g.adicionar(" ", " ", RimnicuVilcea, Pitesti, 97);
-    g.adicionar(" ", " ", RimnicuVilcea, Craiova, 146);
-    g.adicionar(" ", " ", Lugej, Mehadia, 70);
-    g.adicionar(" ", " ", Bucharest, Giurglu, 90);
-    g.adicionar(" ", " ", Bucharest, Urziceni, 85);
-    g.adicionar(" ", " ", Pitesti, Bucharest, 101);
-    g.adicionar(" ", " ", Craiova, Pitesti, 138);
-    g.adicionar(" ", " ", Mehadia, Drobeta, 75);
-    g.adicionar(" ", " ", Drobeta, Craiova, 120);
-    g.adicionar(" ", " ", Urziceni, Vaslui, 142);
-    g.adicionar(" ", " ", Urziceni, Mirsove, 98);
-    g.adicionar(" ", " ", Mirsove, Eforie, 86);
-    g.adicionar(" ", " ", Vaslui, lasi, 92);
-    g.adicionar(" ", " ", lasi, Neamt, 97);
+    g.adicionar("Arad", "Zerind", Arad, Zerind, 75);
+    g.adicionar("Arad", "Sibiu", Arad, Sibiu, 140);
+    g.adicionar("Arad", "Timisoare", Arad, Timisoare, 118);
+    g.adicionar("Zerind", "Oradea", Zerind, Oradea, 71);
+    g.adicionar("Sibiu", "Fagaras", Sibiu, Fagaras, 90);
+    g.adicionar("Sibiu", "RimnicuVilcea", Sibiu, RimnicuVilcea, 80);
+    g.adicionar("Timisoare", "Lugej", Timisoare, Lugej, 111);
+    g.adicionar("Oradea", "Sibiu", Oradea, Sibiu, 151);
+    g.adicionar("Fagaras", "Bucharest", Fagaras, Bucharest, 211);
+    g.adicionar("RimnicuVilcea", "Pitesti", RimnicuVilcea, Pitesti, 97);
+    g.adicionar("RimnicuVilcea", "Craiova", RimnicuVilcea, Craiova, 146);
+    g.adicionar("Lugej", "Mehadia", Lugej, Mehadia, 70);
+    g.adicionar("Bucharest", "Giurglu", Bucharest, Giurglu, 90);
+    g.adicionar("Bucharest", "Urziceni", Bucharest, Urziceni, 85);
+    g.adicionar("Pitesti", "Bucharest", Pitesti, Bucharest, 101);
+    g.adicionar("Craiova", "Pitesti", Craiova, Pitesti, 138);
+    g.adicionar("Mehadia", "Drobeta", Mehadia, Drobeta, 75);
+    g.adicionar("Drobeta", "Craiova", Drobeta, Craiova, 120);
+    g.adicionar("Urziceni", "Vaslui", Urziceni, Vaslui, 142);
+    g.adicionar("Urziceni", "Mirsove", Urziceni, Mirsove, 98);
+    g.adicionar("Mirsove", "Eforie", Mirsove, Eforie, 86);
+    g.adicionar("Vaslui", "lasi", Vaslui, Lasi, 92);
+    g.adicionar("lasi", "Neamt", Lasi, Neamt, 97);
     g.imprimirGrafo(nVertice);
     cout << "--------------------------------" << endl;
     g.buscaEmLargura(Arad, Zerind);
